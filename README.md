560d19ceab87a28d60fe009ce22abd11bc7a2d86
ue <ugurerdal93@gmail.com>
2019-10-19 01:18:20 +0300

    created state to useState converter

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center"> Make useState - React state to useState converter - VScode Extension </h1>
+
+<p align="center"> Easy way to converting all state to useState! 😎</p>
+
+<p align="center"> Visual Studio Code Extension - You can use in multi line or single for converting. Don't waste your time for changing to hook</p>
+<p align="center"><img align="center" src="https://media.giphy.com/media/28evaf0snirPcDCigC/giphy.gif"/></p>
+
+## Install
+
+Click to extension tab and search `Make useState` and install it reload your editor. Or use this [Alphabetical Sorter](https://marketplace.visualstudio.com/items?itemName=ue.make-usestate).
+
+## Usage
+
+Select the desired ones(supports for kind variable).
+Using Command Palette (CMD/CTRL + SHIFT + R) Or
+`Cmd+Shift+P` and select `Make useState` thats it.
+
+## For questions
+
+[Repo](https://github.com/ue/make-usestate)
+
+## License
+
+MIT © [UE](https://github.com/ue)
---
+++ extension.js
@@ -0,0 +1,53 @@
+const vscode = require("vscode");
+
+function activate(context) {
+  const disposable = vscode.commands.registerCommand(
+    "extension.stateToUseState",
+    function() {
+      let editor = vscode.window.activeTextEditor;
+      let lines = [];
+      let selectionLength;
+
+      const selection = editor.selection;
+      const startPoint = selection.start.line;
+      const endPoint = selection.end.line;
+      const isSingleLine = selection.isSingleLine;
+
+      selectionLength = editor.document.lineAt(endPoint).text.length;
+      for (let i = startPoint; i <= endPoint; i++) {
+        const pureLine = editor.document.lineAt(i).text;
+        const selectedText = editor.document.lineAt(i).text.trim();
+        const whiteSpaceCount = pureLine.substr(0, pureLine.indexOf(selectedText.charAt(0)));
+
+        if (selectedText && !selectedText.includes('this.state') && selectedText !== '};') {
+          const dividedLine = selectedText.split(':');
+          const stateName = dividedLine[0] && dividedLine[0].trim();
+          const initialState = dividedLine[1] && dividedLine[1].trim().replace(',', '');
+          const camelCaseStateName = stateName.charAt(0).toUpperCase() + stateName.slice(1);
+
+          if (dividedLine[0].includes(',')) initialState.replace(',', "");
+          const whiteSpace = new Array(whiteSpaceCount).join(" ");
+          const prepareValue = `${whiteSpace}const [${stateName}, set${camelCaseStateName}] = useState(${initialState});`
+
+          lines.push(prepareValue);
+        };
+      }
+
+      editor.edit(editBuilder => {
+        isSingleLine
+          ? editBuilder.replace(selection, lines.join(", "))
+          : editBuilder.replace(
+              new vscode.Range(startPoint, 0, endPoint, selectionLength),
+              lines.join("\n")
+            );
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
